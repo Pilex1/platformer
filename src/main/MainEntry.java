90e9239d058aedd2b4b9d9b8c6cd92b5ebfdd4ff
Alex Tan <torc.madra@hotmail.com>
2018-05-20 02:04:56 +1000

    updated header code

--- src/main/MainEntry.java
+++ src/main/MainEntry.java
@@ -1,12 +1,22 @@
 /*
  * SDD Major Project
  * Version 1.0
- * Apache License (see https://www.apache.org/licenses/LICENSE-2.0)
+ * License: Open Source (please attribute author)
  * 
- * Alex Tan 
+ * 
+ * Author: Alex Tan 
  * April 2018
  * Email: torc.madra@hotmail.com
  * Github: https://github.com/Pilex1/
+ * 
+ * 
+ * 3rd party libraries:
+ * Processing (Casey Reas & Ben Fry)
+ * 
+ * 3rd party images:
+ * Game background (https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/o3UovDg/sci-fi-background_r5cem0n0_thumbnail-full01.png)
+ * 
+ * All other code and images is created by the author
  * 
  */
 
